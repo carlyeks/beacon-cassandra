d0c1d5ec9f9c29f1ff0eb907e1169422b2bf255c
Carl Yeksigian <carl@yeksigian.com>
2014-04-02 21:44:12 -0400

    Initial Commit

---
+++ src/main/scala/org/yksgn/beakin/Import.scala
@@ -0,0 +1,34 @@
+/**
+  *     Copyright (C) 2014. Carl Yeksigian.
+  * 
+  *     This program is free software: you can redistribute it and/or modify
+  *     it under the terms of the GNU General Public License as published by
+  *     the Free Software Foundation, either version 3 of the License, or
+  *     (at your option) any later version.
+  * 
+  *     This program is distributed in the hope that it will be useful,
+  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *     GNU General Public License for more details.
+  * 
+  *     You should have received a copy of the GNU General Public License
+  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package org.yksgn.beakin
+
+import com.datastax.driver.core
+import edu.berkeley.cs.amplab.adam.avro.ADAMRecord
+import edu.berkeley.cs.amplab.adam.projections.ADAMRecordField._
+import edu.berkeley.cs.amplab.adam.projections.Projection
+import edu.berkeley.cs.amplab.adam.rdd.AdamContext._
+import edu.berkeley.cs.amplab.adam.rdd.AdamContext
+import parquet.filter.UnboundRecordFilter
+
+object Import {
+  def exec(args: Array[String]) {
+    val sc = AdamContext.createSparkContext("beakin: import", "local[4]", null, Seq(), Seq())
+    val proj = Projection(referenceName, referenceUrl, start, sequence, readMapped, primaryAlignment, firstOfPair)
+    args.map(sc.adamLoad[ADAMRecord, UnboundRecordFilter](_, projection=Some(proj)))
+  }
+}
