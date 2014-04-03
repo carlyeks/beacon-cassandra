95cbf0e76f28f7e091389e5e98ec6a9908309a2a
Carl Yeksigian <carl@yeksigian.com>
2014-04-03 07:35:16 -0400

    Using foreachPartiton to save from the partitions

--- beacon-import/src/main/scala/org/yksgn/beacon/Main.scala
+++ beacon-import/src/main/scala/org/yksgn/beacon/Main.scala
@@ -1,18 +1,18 @@
 /**
-  *     Copyright (C) 2014. Carl Yeksigian.
-  * 
-  *     This program is free software: you can redistribute it and/or modify
-  *     it under the terms of the GNU General Public License as published by
-  *     the Free Software Foundation, either version 3 of the License, or
-  *     (at your option) any later version.
-  * 
-  *     This program is distributed in the hope that it will be useful,
-  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *     GNU General Public License for more details.
-  * 
-  *     You should have received a copy of the GNU General Public License
-  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  * Copyright (C) 2014. Carl Yeksigian.
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
 
 package org.yksgn.beacon
@@ -26,20 +26,7 @@ import edu.berkeley.cs.amplab.adam.rdd.AdamContext
 import parquet.filter.UnboundRecordFilter
 
 object Main extends App {
-  var cluster : Cluster = null
-
-  private def connectCassandra(node: String) : Session = {
-    if (cluster == null) {
-      cluster = Cluster.builder()
-        .addContactPoint(node)
-        .build()
-    }
-    cluster.connect()
-  }
-
-  private def close() {
-    cluster.close()
-  }
+  val query = "INSERT INTO beacon.locations (chromosome, location, base) VALUES (?, ?, ?)"
 
   override def main(args: Array[String]) {
     val sc = AdamContext.createSparkContext("beacon: import", "local[4]", null, Seq(), Seq())
@@ -49,7 +36,19 @@ object Main extends App {
       .flatMap(ar => ar.getSequence.zipWithIndex.map{ case (s, idx) => (ar.getReferenceName, ar.getStart + idx, s) })
       .cache()
 
-    val query = "INSERT INTO beacon.locations (chromosome, location, base) VALUES (?, ?, ?)"
+    allRecords.foreachPartition(partition => {
+      val cluster = Cluster.builder()
+        .addContactPoint("localhost")
+        .build()
+      try {
+        val session = cluster.connect()
 
+        val prepared = session.prepare(query)
+
+        partition.foreach { case (ref, loc, base) => session.execute(prepared.bind(ref, Long.box(loc), base.toString)) }
+      } finally {
+        cluster.close()
+      }
+    })
  }
 }
